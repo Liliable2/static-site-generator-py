769014dc3d143dc1c099aef7fb708654f3255177
Liliable2 <nattanan.char@mail.kmutt.ac.th>
2026-01-04 13:20:11 +0700

    feat: add ParentNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -41,6 +41,63 @@ class TestLeafNode(unittest.TestCase):
 
     def test_to_html_no_value_raises_error(self):
         node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("b", "Bold text")
+        parent_node = ParentNode("p", [child_node])
+        self.assertEqual(parent_node.to_html(), "<p><b>Bold text</b></p>")
+
+    def test_to_html_with_multiple_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_props(self):
+        node = ParentNode(
+            "div",
+            [LeafNode("span", "child")],
+            {"class": "container", "id": "main"},
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<div class="container" id="main"><span>child</span></div>',
+        )
+
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, [LeafNode("b", "bold")])
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_no_children(self):
+        node = ParentNode("div", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_empty_children(self):
+        node = ParentNode("div", [])
         with self.assertRaises(ValueError):
             node.to_html()
 
